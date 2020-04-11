0f27549b6632fb9d5291900eff19048a9c85f516
mr-khan <mahfuzku11@gmail.com>
2020-04-12 01:29:41 +0600

    [subscription_api] add vue app with routing problem

---
+++ subscription-api/subscription-app/src/main.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import * as VeeValidate from 'vee-validate';
+// import * as VueThreejs from '@'
+// import VueThreejs from '@'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+// Vue.use(VueThreejs)
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
