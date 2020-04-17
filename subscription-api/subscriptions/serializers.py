d322821f1f49bb1198b3b5bb30b0b89a310c5a22
mahfuz <mahfuz@misfit.tech>
2020-04-17 17:23:00 +0600

    [susbscription_app] update gitignore file

--- subscription-api/subscriptions/serializers.py
+++ subscription-api/subscriptions/serializers.py
@@ -6,6 +6,6 @@ from .models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ('name', 'description', 'currency',
+        fields = ('id', 'name', 'description', 'currency',
                   'amount', 'created_at', 'updated_at'
                   )
