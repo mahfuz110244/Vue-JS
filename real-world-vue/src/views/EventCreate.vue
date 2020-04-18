58a6c3ba59aab43c143a61456013f65e362477b5
mahfuz110244 <mahfuzku11@gmail.com>
2020-04-19 03:40:48 +0600

    lesson9-api-call-with-axios-start-v1 load graphSQL server fpr API data

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Event Create</h1>
+    <BaseButton>Update</BaseButton>
+    <!-- <BaseButton>Purchase for 100</BaseButton> -->
+    <!-- <BaseButton>Purchase for ${{ total }}</BaseButton> -->
+    <MediaBox>
+      <h2 slot="heading">Adam Jahr</h2>
+      <BaseIcon name="book"></BaseIcon>
+      <p slot>My words.</p>
+    </MediaBox>
+  </div>
 </template>
--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
 
 export default {
   components: {
     EventCard
+  },
+
+  data() {
+    return {
+      events: []
+    };
+  },
+
+  created() {
+    axios
+      .get("http://localhost:3000/events") // Does a get request
+      .then(response => {
+        this.events = response.data;
+        console.log(response.data); // For now, logs out the response
+      })
+      .catch(error => {
+        console.log("There was an error:", error.response); // Logs out the error
+      });
   }
 };
 </script>
